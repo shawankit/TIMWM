c13f8b987107884654e7b5f91b667fa9e1cfdae1
Ankit <ankit.shaw@napses.com>
2023-04-01 20:36:11 +0530

    reveriting fix

--- functions/migrations/20230224183405-add-unique-constraint-to-tables.js
+++ functions/migrations/20230224183405-add-unique-constraint-to-tables.js
@@ -1,17 +1,17 @@
 
 module.exports = {
   async up (queryInterface) {
-      // await queryInterface.addConstraint('ledgers', {
-      //     type: 'UNIQUE',
-      //     fields: ['name'],
-      //     name: 'ledgers_name_key'
-      // });
+      await queryInterface.addConstraint('ledgers', {
+          type: 'UNIQUE',
+          fields: ['name'],
+          name: 'ledgers_name_key'
+      });
 
-      // await queryInterface.addConstraint('ledgers', {
-      //     type: 'UNIQUE',
-      //     fields: ['code'],
-      //     name: 'ledgers_code_key'
-      // });
+      await queryInterface.addConstraint('ledgers', {
+          type: 'UNIQUE',
+          fields: ['code'],
+          name: 'ledgers_code_key'
+      });
 
     //   await queryInterface.addConstraint('customers', {
     //     type: 'UNIQUE',
